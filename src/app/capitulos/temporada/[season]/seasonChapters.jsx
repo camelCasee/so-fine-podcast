00bd6e02036184a3ccda9bd8ce4265be8396da28
Ignacio Nuñez <ignacio.antonio420@gmail.com>
2023-04-28 19:37:49 +0200

    chapters test

--- src/app/capitulos/temporada/[season]/seasonChapters.jsx
+++ src/app/capitulos/temporada/[season]/seasonChapters.jsx
@@ -1,18 +1,11 @@
 import retrieveSeasonChapters from "@/logic/retrieveSeasonChapters"
 
 export default async function SeasonChapters({ season }) {
-    let error 
     const chapters = await retrieveSeasonChapters(season)
-        .catch(err =>  {
-            console.error('failed to read file', err)
-
-            error = err
-        })
-        
+        .catch(err =>  console.error('failed to read file', err))
 
     return <div className="px-7">
         <p>{season}</p>
-        <p>{error}</p>
         <ul className="responsive-grid gap-y-4 items-center">
             {chapters?.map(chapter => {
                 return <li key={chapter.title + season} className="px-2 max-w-[18rem] justify-self-center flex flex-col items-center">
