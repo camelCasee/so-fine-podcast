1ceee8ae53781df27e98522ce89cc1875a012aae
Ignacio Nuñez <ignacio.antonio420@gmail.com>
2023-05-25 22:08:37 +0200

    test

--- src/logic/retrieveSeasonChapters.js
+++ src/logic/retrieveSeasonChapters.js
@@ -1,10 +1,10 @@
 import { promises as fs} from 'fs'
 
 export default async function retrieveSeasonChapters(season){
-    const directory = `./chapters/season-${season}.json`
+    const directory = `./src/chapters/season-${season}.json`
 
     const data = await fs.readFile(directory, 'utf8')
-
+  
     const chapters = JSON.parse(data)
 
     return chapters
